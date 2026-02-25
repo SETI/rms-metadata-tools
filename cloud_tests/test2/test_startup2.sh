dec1da466b0b124c904c9158db0587ffecac1747
Joseph Spitale <jspitale@seti.org>
2026-02-25 13:56:55 -0700

    degbugging

--- cloud_tests/test2/test_startup2.sh
+++ cloud_tests/test2/test_startup2.sh
@@ -1,16 +1,12 @@
 apt-get update -y
 apt-get install -y python3 python3-pip python3-venv git
 cd
-if [ ! -d rms-metadata-tools ]; then
-#  git clone https://github.com/SETI/rms-metadata-tools.git
-  git clone -b jns--updates-continued --single-branch https://github.com/SETI/rms-metadata-tools.git
-  cd rms-metadata-tools
-  python3 -m venv venv
-  source venv/bin/activate
-  pip install -r requirements-cloud.txt
-else
-  cd rms-metadata-tools
-fi
+#git clone https://github.com/SETI/rms-metadata-tools.git
+git clone -b jns--updates-continued --single-branch https://github.com/SETI/rms-metadata-tools.git
+cd rms-metadata-tools
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements-cloud.txt
 
 cd cloud_tests/test2
 python3 cloud_test2.py
