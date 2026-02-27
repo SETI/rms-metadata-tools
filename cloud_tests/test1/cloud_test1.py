5261783a07d9b9b12d92e43014212a890dce2af7
Joseph Spitale <jspitale@seti.org>
2026-02-27 14:44:49 -0700

    debugging

--- cloud_tests/test1/cloud_test1.py
+++ cloud_tests/test1/cloud_test1.py
@@ -17,47 +17,32 @@
 # gsutil ls gs://rms-metadata-jspitale/test.txt
 # gsutil cat gs://rms-metadata-jspitale/test.txt
 
+
+
+import asyncio
+import sys
+from cloud_tasks.worker import Worker, WorkerData
 from filecache import FCPath
-
-filespec = FCPath('gs://rms-metadata-jspitale/test.txt')
-if filespec.exists():
-    content = filespec.read_text(encoding='utf-8')
-    filespec.write_text(content + '\nGoodbye\nWorld\n', encoding='utf-8')
-    exit()
-filespec.write_text('Hello\nWorld\n', encoding='utf-8')
-exit()
-
-
-
-
-
-
-#mport asyncio
-#import sys
-#from cloud_tasks.worker import Worker, WorkerData
-#from filecache import FCPath
 
 #import metadata_tools.util as util
 
 #========================================================================================
-#def process_task(task_id: str,
-#                 task_data: dict[str, any],
-#                 worker_data: WorkerData) -> tuple[bool, any]:
+def process_task(task_id: str,
+                 task_data: dict[str, any],
+                 worker_data: WorkerData) -> tuple[bool, any]:
 
 
-#    filespec = FCPath('gs://rms-metadata-jspitale/test.txt')
-##    util.write_txt_file(filespec, ['Hello', 'World'])
-#    filespec.write_text(f"Hello from {worker_id}\n")
-#    return False, None
+    filespec = FCPath('gs://rms-metadata-jspitale/test.txt')
+#    util.write_txt_file(filespec, ['Hello', 'World'])
+    filespec.write_text(f"Hello from {worker_id}\n")
+    return False, None
 
 #========================================================================================
-#async def main():
-###    filespec = FCPath('gs://rms-metadata-jspitale/xxxtest.txt')
-###    filespec.write_text('Hey\nYo\n', encoding='utf-8')
+async def main():
 
-#    worker = Worker(process_task, args=sys.argv[1:])
-#    await worker.start()
+    worker = Worker(process_task, args=sys.argv[1:])
+    await worker.start()
 
 #########################################################################################
-#if __name__ == "__main__":
-#    asyncio.run(main())
+if __name__ == "__main__":
+    asyncio.run(main())
