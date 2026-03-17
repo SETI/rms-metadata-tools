8dd21faa8020958faa72905f7266da6cddc32b2a
Joseph Spitale <jspitale@seti.org>
2026-03-17 12:40:25 -0700

    db

--- cloud_tests/test1/cloud_test1.py
+++ cloud_tests/test1/cloud_test1.py
@@ -24,11 +24,11 @@ import multiprocessing
 import sys
 from cloud_tasks.worker import Worker, WorkerData
 from filecache import FCPath
-#from IPython import embed; print('+++++++++++++'); embed()
 
 import sys
 sys.path.append('')
 
+from IPython import embed; print('+++++++++++++'); embed()
 #import metadata_tools.util as util
 
 #========================================================================================
