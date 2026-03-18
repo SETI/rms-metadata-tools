3c3d07f186aad61b8682e308f0a1f44d82f9044f
Joseph Spitale <jspitale@seti.org>
2026-03-18 12:59:48 -0700

    db

--- cloud_tests/test1/test1_startup.sh
+++ cloud_tests/test1/test1_startup.sh
@@ -1,6 +1,6 @@
 apt-get update -y
 apt-get install -y python3 python3-pip python3-venv git
-cd || exit 1
+cd
 #git clone https://github.com/SETI/rms-metadata-tools.git
 git clone -b jns--updates-continued --single-branch https://github.com/SETI/rms-metadata-tools.git
 cd rms-metadata-tools
