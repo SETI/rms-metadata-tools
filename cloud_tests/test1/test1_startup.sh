f0db6bc1fc22f9fb6f71bc10631e69ede4c752f3
Joseph Spitale <jspitale@seti.org>
2026-03-18 12:44:17 -0700

    db

--- cloud_tests/test1/test1_startup.sh
+++ cloud_tests/test1/test1_startup.sh
@@ -1,11 +1,11 @@
 apt-get update -y
 apt-get install -y python3 python3-pip python3-venv git
 cd || exit 1
-#git clone https://github.com/SETI/rms-metadata-tools.git || exit 1
-git clone -b jns--updates-continued --single-branch https://github.com/SETI/rms-metadata-tools.git || exit 1
-cd rms-metadata-tools || exit 1
-python3 -m venv venv || exit 1
-source venv/bin/activate || { echo 'No virtual environment' ; exit 1; }
+#git clone https://github.com/SETI/rms-metadata-tools.git
+git clone -b jns--updates-continued --single-branch https://github.com/SETI/rms-metadata-tools.git
+cd rms-metadata-tools
+python3 -m venv venv
+source venv/bin/activate
 pip install -r requirements-cloud.txt
 
 ###cd cloud_tests/test1
