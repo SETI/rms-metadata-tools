afa774d0363537f3e5ad571169e4dd2be6e9878d
Joseph Spitale <jspitale@seti.org>
2026-03-24 09:41:36 -0700

    db

--- metadata_tools/hosts/GO_0xxx/gcp_index_startup.sh
+++ metadata_tools/hosts/GO_0xxx/gcp_index_startup.sh
@@ -13,7 +13,6 @@ pip install -r requirements.txt
 python3 metadata_tools/hosts/GO_0xxx/GO_0xxx_index_cloud.py \
                 gs://rms-node-holdings/pds3-holdings/volumes/GO_0xxx/ \
                 gs://rms-node-metadata/GO_0xxx/ \
-                gs://rms-metadata-jspitale/metadata_test/GO_000x
-
+                gs://rms-metadata-jspitale/metadata_test/GO_0xxx/
 
 
