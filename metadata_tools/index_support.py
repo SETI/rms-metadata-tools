1c0921f2ee9294670e1a6118616fd0bbdb336aec
Joseph Spitale <jspitale@seti.org>
2026-03-25 12:48:21 -0700

    db

--- metadata_tools/index_support.py
+++ metadata_tools/index_support.py
@@ -549,7 +549,7 @@ def _create_index(volume_tree, output_tree, template_path, metadata_tree=None,
                 # ... or process this volumne
                 else:
                     # Process this volumne
-                    util.gsprint(vol)
+#                    util.gsprint(vol)
                     index = IndexTable(indir, outdir, template_path, metadata_dir,
                                        qualifier=qualifier, volume_id=vol, glob=glob)
                     index.create(labels_only=labels_only, pattern=pattern)
