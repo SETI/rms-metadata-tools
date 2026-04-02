162f991d4a8120ee9c9cef9796c7e1b25017730b
Joseph Spitale <jspitale@seti.org>
2026-04-02 09:33:35 -0700

    db

--- metadata_tools/util.py
+++ metadata_tools/util.py
@@ -23,7 +23,7 @@ def dbprint(message):
 
     Returns: None
     """
-    time = dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    time = dt.datetime.now()#.strftime('%Y-%m-%d %H:%M:%S')
     print(f'{time} - {message}', file=sys.stderr, flush=True)
 
 #===============================================================================
