8917635c179dab0bc81119094c600579b01a5de8
Joseph Spitale <jspitale@seti.org>
2026-04-23 10:27:07 -0700

    db

--- metadata_tools/hosts/GO_0xxx/gcp_geometry_startup.sh
+++ metadata_tools/hosts/GO_0xxx/gcp_geometry_startup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# source "${BASH_SOURCE%/*}gcp_startup_common.sh
+source "${BASH_SOURCE%/*}gcp_startup_common.sh
 python3 metadata_tools/hosts/GO_0xxx/GO_0xxx_geometry_cloud.py \
                 gs://rms-node-holdings/pds3-holdings/metadata/GO_0xxx/ \
                 gs://rms-metadata-jspitale/metadata_test/GO_0xxx/
