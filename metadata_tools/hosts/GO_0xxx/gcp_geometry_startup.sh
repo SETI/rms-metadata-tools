e5b76e147115fbf7b2f861ab57e55dfa69fb9f29
Joseph Spitale <jspitale@seti.org>
2026-04-23 14:35:49 -0700

    db

--- metadata_tools/hosts/GO_0xxx/gcp_geometry_startup.sh
+++ metadata_tools/hosts/GO_0xxx/gcp_geometry_startup.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
-source "${BASH_SOURCE%/*}gcp_startup_common.sh
+
+apt-get update -y
+apt-get install -y python3 python3-pip python3-venv git
+cd
+#git clone https://github.com/SETI/rms-metadata-tools.git
+git clone -b jns--updates-continued --single-branch https://github.com/SETI/rms-metadata-tools.git
+cd rms-metadata-tools
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+
+##cd hosts/GO_0xxx
+
+## -------------------------------------------------------
+## Additional instance shell commands
+##  gcloud auth application-default login
+##  export GCLOUD_PROJECT="rms-metadata"
+## -------------------------------------------------------
+
 python3 metadata_tools/hosts/GO_0xxx/GO_0xxx_geometry_cloud.py \
                 gs://rms-node-holdings/pds3-holdings/metadata/GO_0xxx/ \
                 gs://rms-metadata-jspitale/metadata_test/GO_0xxx/
