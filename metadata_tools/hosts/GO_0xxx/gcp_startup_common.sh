38fd5ca95b98d257557b50cf858039277ef56e31
Joseph Spitale <jspitale@seti.org>
2026-04-23 13:53:28 -0700

    db

--- metadata_tools/hosts/GO_0xxx/gcp_startup_common.sh
+++ metadata_tools/hosts/GO_0xxx/gcp_startup_common.sh
@@ -1,12 +1,13 @@
-#apt-get update -y
-#apt-get install -y python3 python3-pip python3-venv git
-#cd
-##git clone https://github.com/SETI/rms-metadata-tools.git
-#git clone -b jns--updates-continued --single-branch https://github.com/SETI/rms-metadata-tools.git
-#cd rms-metadata-tools
-#python3 -m venv venv
-#source venv/bin/activate
-#pip install -r requirements.txt
+#!/bin/bash
+apt-get update -y
+apt-get install -y python3 python3-pip python3-venv git
+cd
+#git clone https://github.com/SETI/rms-metadata-tools.git
+git clone -b jns--updates-continued --single-branch https://github.com/SETI/rms-metadata-tools.git
+cd rms-metadata-tools
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
 
 ##cd hosts/GO_0xxx
 
