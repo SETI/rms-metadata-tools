48537587616df72b13e7733e60fad8591ee88c41
Joseph Spitale <jspitale@seti.org>
2026-04-28 12:25:03 -0700

    db

--- metadata_tools/geometry_support.py
+++ metadata_tools/geometry_support.py
@@ -960,7 +960,6 @@ class Record(object):
         # Write the formatted value(s)
         strings = []
         for number in results:
-            print(number)
             error_message = ""
 
             # numeric values: flag common exceptions and use standard format
