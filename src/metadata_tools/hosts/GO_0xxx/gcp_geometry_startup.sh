b3804ecca6521e044ace8234b9562a966dbe7f2e
Joseph Spitale <jspitale@seti.org>
2026-07-06 14:41:33 -0700

    db

--- src/metadata_tools/hosts/GO_0xxx/gcp_geometry_startup.sh
+++ src/metadata_tools/hosts/GO_0xxx/gcp_geometry_startup.sh
@@ -34,5 +34,5 @@ metadata-geometry-cloud GO_0xxx \
 gcloud auth application-default login
 metadata-geometry GO_0xxx \
                 gs://rms-node-holdings/pds3-holdings/metadata/GO_0xxx/ \
-                gs://rms-metadata-jspitale/metadata_test/GO_0xxx/ -vv GO_0022
+                gs://rms-metadata-jspitale/metadata_test/GO_0xxx/ -vv GO_0022 --num-simultaneous-tasks 1
 COMMENT_BLOCK
